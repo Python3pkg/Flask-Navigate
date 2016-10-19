23002ba8f3238ac3ed2e15aa6c22e5ab80f1b053
Bill Schumacher <bill@servernet.co>
2016-10-19 18:04:31 -0500

    Some helper functions to populate forms and update objects, since it seems the wtfalchemy may have broken form population from objects, doesn't seem to work.
    Add edit nav endpoint url to nav hyperlink in nav list.
    
    Add dot helper function, concat by .
    
    Add flash messages to edit.

--- flask_navigate/views.py
+++ flask_navigate/views.py
@@ -1,21 +1,28 @@
 from flask_bs import render_content_with_bootstrap
-from flask import current_app, redirect, request, url_for
-from flask_navigate.models import Nav, NavForm
+from flask import current_app, redirect, request, url_for, flash
 from werkzeug.local import LocalProxy
 from flask_wtf_flexwidgets import render_form_template, css_template
 from jinja2 import Template
-
+from ._compat import iteritems
+from .models import Nav, NavForm
 
 _navigate = LocalProxy(lambda: current_app.extensions['navigate'])
 
 _datastore = LocalProxy(lambda: _navigate.datastore)
 
 
+def dot(a, b):
+    return "{}.{}".format(a, b)
+
+
 def admin_list_nav():
     navigation_menus = _datastore.get_all_nav()
     return render_content_with_bootstrap(body=nav_admin_list_template.
                                          render(navs=navigation_menus,
-                                                add_nav_endpoint=_navigate.blueprint_name + '.admin_add_nav',
+                                                add_nav_endpoint=dot(_navigate.blueprint_name,
+                                                                     _navigate.admin_add_nav_endpoint),
+                                                edit_nav_endpoint=dot(_navigate.blueprint_name,
+                                                                      _navigate.admin_edit_nav_endpoint),
                                                 url_for=url_for),
                                          head="<style>" + css_template + "</style>")
 
@@ -36,22 +43,40 @@ def admin_add_nav():
                                              head="<style>" + css_template + "</style>")
 
 
+def populate_form(form, obj):
+    for key in form.data_without_submit.keys():
+        if key in obj.__table__.columns.keys():
+            form.__getattribute__(key).data = obj.__getattribute__(key)
+
+
+def update_object(form, obj):
+    dirty = False
+    for key, value in iteritems(form.data_without_submit):
+        if key in obj.__table__.columns.keys():
+            obj.__setattr__(key, value)
+            dirty = True
+    if dirty:
+        _datastore.commit()
+
+
 def admin_edit_nav(nav_id=None):
     nav_obj = _datastore.get_nav(nav_id)
     if nav_obj:
         form = NavForm()
-        form.process(obj=nav_obj)
+        populate_form(form, nav_obj)
         if request.method == 'GET':
             rendered_form = render_form_template(form)
             return render_content_with_bootstrap(body=nav_admin_edit_nav_template.render(form=rendered_form),
                                                  head="<style>" + css_template + "</style>")
         else:
             if form.validate():
-                print(dir(nav_obj))
+                flash("Nav menu updated", "success")
                 return redirect(url_for(_navigate.blueprint_name + '.admin_list_nav'))
             rendered_form = render_form_template(form)
             return render_content_with_bootstrap(body=nav_admin_edit_nav_template.render(form=rendered_form),
                                                  head="<style>" + css_template + "</style>")
+    flash("Nav menu not found", "error")
+    return redirect(url_for(dot(_navigate.blueprint_name, _navigate.admin_list_nav_endpoint)))
 
 
 def admin_delete_nav():
@@ -75,7 +100,7 @@ nav_admin_list_template = Template("""
     <div>
         <h4>Navigation Menus</h4>
     {%- for nav in navs -%}
-        <div><a href="#">{{ nav.name }}</a> -=- <a href="#">Delete</a></div>
+        <div><a href="{{ url_for(edit_nav_endpoint, nav_id=nav.id) }}">{{ nav.name }}</a> -=- <a href="#">Delete</a></div>
     {% else %}
         <div>No menus created yet.</div>
     {%- endfor -%}
